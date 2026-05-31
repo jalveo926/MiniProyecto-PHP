9f867e948c81ff3ec5ec67bff488b3544a845e22
rquintero14 <rquinte14@gmail.com>
2026-05-31 01:01:24 -0500

    Se agrega la logica del problema 4

--- app/controllers/Problema4Controller.php
+++ app/controllers/Problema4Controller.php
@@ -1,13 +1,44 @@
 <?php
 
-use App\Utils\Validador;
-use App\Utils\Sanitizador;
 use App\Utils\Matematica;
+use App\Utils\Navigation;
 
-$resultado = null;
-$error = null;
+/**
+ * Controlador del Problema 4
+ * Calcula independientemente la suma de números pares
+ * e impares comprendidos entre 1 y 200.
+ * Utiliza switch para seleccionar el tipo de visualización.
+ */
 
-// TODO: Implementar lógica del Problema 4
+$LIMITE = 200;
+$sumaPares   = 0;
+$sumaImpares = 0;
+$pares       = [];
+$impares     = [];
+
+// Recorrer del 1 al 200 y clasificar cada número
+for ($i = 1; $i <= $LIMITE; $i++) {
+    switch ($i % 2) {
+        case 0: // Es par
+            $sumaPares += $i;
+            $pares[]    = $i;
+            break;
+        default: // Es impar
+            $sumaImpares += $i;
+            $impares[]    = $i;
+            break;
+    }
+}
+
+$resultado = [
+    'sumaPares'    => $sumaPares,
+    'sumaImpares'  => $sumaImpares,
+    'totalPares'   => count($pares),
+    'totalImpares' => count($impares),
+    'sumaTotal'    => $sumaPares + $sumaImpares,
+    'pares'        => $pares,
+    'impares'      => $impares,
+];
 
 include __DIR__ . '/../views/problema4.php';
 ?>
--- app/views/menu.php
+++ app/views/menu.php
@@ -10,7 +10,7 @@
         <li><a href="index.php?action=problema1">Problema 1: Estadísticas de 5 números positivos</a></li>
         <li><a href="index.php?action=problema2">Problema 2: Suma de 1 a 1000</a></li>
         <li><a href="index.php?action=problema3">Problema 3: Presupuesto del Hospital</a></li>
-        <li><a href="index.php?action=problema4">Problema 4: [Por implementar]</a></li>
+        <li><a href="index.php?action=problema4">Problema 4: Suma de Pares e Impares del 1 al 200</a></li>
         <li><a href="index.php?action=problema5">Problema 5: Clasificación de Personas por Edad</a></li>
         <li><a href="index.php?action=problema6">Problema 6: [Por implementar]</a></li>
         <li><a href="index.php?action=problema7">Problema 7: Calculadora de Datos Estadísticos</a></li>
